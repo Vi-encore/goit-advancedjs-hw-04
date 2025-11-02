361bd3157fc78cdeab50f46c81d706b87d4969a1
Viktoriia Matiushko <vikamatuchko772@gmail.com>
2025-11-02 15:34:03 +0200

    deleted old code

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,20 +3,6 @@ import axios from 'axios';
 const API_KEY = '24152486-dede519cf70640e98e72cca28';
 axios.defaults.baseURL = 'https://pixabay.com/api';
 
-// export default function fetchPic(value) {
-//   const searchParams = new URLSearchParams({
-//     key: '50950053-db3558a1af50bab2399c0c009',
-//     q: value,
-//     image_type: 'photo',
-//     orientation: 'horizontal',
-//     safesearch: true,
-//   });
-//   return fetch(`https://pixabay.com/api/?${searchParams}`).then(res => {
-//     if (!res.ok) throw new Error(response.status);
-//     return res.json();
-//   });
-// }
-
 export default async function fetchPic(
   searchParam,
   page = 1,
