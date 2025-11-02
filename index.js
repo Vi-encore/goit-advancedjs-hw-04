9c22805f4eef0803df73bd94ab4b4ccb2dcd4b62
Vi-encore <73960387+Vi-encore@users.noreply.github.com>
2025-11-02 12:25:00 +0000

    Deploying to gh-pages from @ Vi-encore/goit-advancedjs-hw-04@21075096531669df8d364b96de84068b2f1581c5 🚀

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import{S as f,i as l}from"./assets/vendor-CePATv9e.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const i of r.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&a(i)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function a(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();function g(o){const t=new URLSearchParams({key:"50950053-db3558a1af50bab2399c0c009",q:o,image_type:"photo",orientation:"horizontal",safesearch:!0});return fetch(`https://pixabay.com/api/?${t}`).then(s=>{if(!s.ok)throw new Error(response.status);return s.json()})}function h(o){let t="";return o.map(s=>{const{webformatURL:a,largeImageURL:e,tags:r,likes:i,views:p,comments:m,downloads:u}=s;t+=`<li class='gallery_item'>
     <a href='${e}' target="_blank">
-      <img src='${a}' alt='${r}' class='gallery_img'/>
+      <img src='${a}' alt='${r}' class='gallery_img test'/>
       <div class='gallery_info_wrapper'>
         <p class='gallery_info'>Likes <span>${i}</span></p>
         <p class='gallery_info'>Views <span>${p}</span></p>
